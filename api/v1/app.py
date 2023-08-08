443bbf9ae96c3501115eb666702224ed425ab7ac
TriremeSolutions <themellowread@gmail.com>
2023-08-08 16:08:37 +0100

    import getenv fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import make_response
 from flask import jsonify
+from os import getenv
 import models
 from models import storage
 from api.v1.views import app_views
