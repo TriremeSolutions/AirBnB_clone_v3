891a1a1a61c0d33e21220cfd4a1871bb5e200491
TriremeSolutions <themellowread@gmail.com>
2023-08-08 13:22:30 +0100

    implement get and count unittests successfully

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 Contains the TestFileStorageDocs classes
 """
 
+import unittest
 from datetime import datetime
 import inspect
 import models
@@ -17,7 +18,6 @@ from models.user import User
 import json
 import os
 import pep8
-import unittest
 FileStorage = file_storage.FileStorage
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
@@ -113,3 +113,20 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+
+    # implement tests for _get and _count methods
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Test that the requested object is returned"""
+        storage = FileStorage()
+        user = User(name="RandomUser")
+        user.save()
+        self.assertEqual(user, storage.get("User", user.id))
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test that all objects are counted properly"""
+        storage = FileStorage()
+        obj_count = len(storage._FileStorage__objects)
+        self.assertEqual(obj_count, storage.count())
