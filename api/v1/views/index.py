d474bb110722f6959488a2c9255e4a18c3338987
TriremeSolutions <themellowread@gmail.com>
2023-08-11 13:08:45 +0100

    Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,15 @@ from models import storage
 def status():
     """returns a json file"""
     return jsonify(status="OK")
+
+
+# Create an endpoint that retrieves the number of each objects by type:
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    "Retrieve number of each type of object/instance"
+    return jsonify(users=storage.count("User"),
+                   states=storage.count("State"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   amenities=storage.count("Amenity"),
+                   reviews=storage.count("Review"))
