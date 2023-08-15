4e5d68b5435f9880c8e9dc400541544fc4631bee
TriremeSolutions <themellowread@gmail.com>
2023-08-15 02:41:12 +0100

    test update places_search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -99,82 +99,133 @@ def handle_place_by_id(place_id):
 
 @app_views.route('/places_search', methods=['POST'],
                  strict_slashes=False)
+# def places_search():
+#     """
+#     Retrieves all Place objects using JSON in request body
+#     """
+#     # If the HTTP request body is not valid JSON,
+#     # raise a 400 error with the message Not a JSON
+
+#     bod_req = request.get_json()
+#     if bod_req is None:
+#         abort(400, "Not a JSON")
+
+#     # If the JSON body is empty, or each list of all keys are empty:
+#     # retrieve all Place objects
+#     st = bod_req['states']
+#     cy = bod_req['cities']
+#     amn = bod_req.get('amenities')
+#     # recall that these are lists of ids, not actual names
+
+#     if not bod_req or (not st and not cy and not amn):
+#         plc_list = storage.all(Place)
+#         return jsonify([p.to_dict() for p in plc_list.values()])
+
+#     # implicit else, return empty json.
+#     plc_list = []
+#     # also a proactive else for future if-conditions, as well
+
+#     # If states list is not empty, results should include all Place objects
+#     # for each State id listed
+
+#     if st and len(st) > 0:
+#         st_list = [storage.get("State", n) for n in st]
+#         # json file of state names, including their ids
+
+#         # iterate for every state in the states
+#         for x in st_list:
+#             # of every city in that state
+#             for y in x.cities:
+#                 # for every place in that city
+#                 [plc_list.append(z) for z in y.places]
+#                 # add to the list of places and return as json,
+#                 # after all other if-blocks
+
+#     if cy and len(cy) > 0:
+#         cy_list = [storage.get("City", n) for n in cy]
+#         for x in cy_list:
+#             [plc_list.append(y) for y in x.places if y not in plc_list]
+
+#     if not plc_list:
+#         plc_list = [n for n in storage.all(Place).values()]
+
+#     # If amenities list is not empty, limit search results
+#     # to only Place objects having all Amenity ids listed
+
+#     if amn:
+#         # create list of valid amenity instances
+#         amn_list = [storage.get("Amenity", n) for n in amn]
+#         count = 0
+#         limit = len(plc_list)
+#         HBNB_API_HOST = getenv('HBNB_API_HOST')
+#         HBNB_API_PORT = getenv('HBNB_API_PORT')
+
+#         port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
+#         url = "http://0.0.0.0:{}/api/v1/places/".format(port)
+#         while count < limit:
+#             place = plc_list[count]
+#             url = url + '{}/amenities'
+#             req = url.format(place.id)
+#             res = requests.get(req)
+#             jlod = json.loads(res.text)
+#             amen = [storage.get("Amenity", x['id']) for x in jlod]
+#             for a in amn_list:
+#                 if a not in amen:
+#                     plc_list.pop(count)
+#                     count -= 1
+#                     limit -= 1
+#                     break
+#             count += 1
+#     # the proactive else
+#     return jsonify([p.to_dict() for p in plc_list])
+
 def places_search():
     """
-    Retrieves all Place objects using JSON in request body
+        Retrieves all Place objects using JSON in request body
     """
-    # If the HTTP request body is not valid JSON,
-    # raise a 400 error with the message Not a JSON
-
+    plc_list = [p for p in storage.all('Place').values()]
     bod_req = request.get_json()
     if bod_req is None:
-        abort(400, "Not a JSON")
-
-    # If the JSON body is empty, or each list of all keys are empty:
-    # retrieve all Place objects
-    st = bod_req['states']
-    cy = bod_req['cities']
-    amn = bod_req.get('amenities')
-    # recall that these are lists of ids, not actual names
-
-    if not bod_req or (not st and not cy and not amn):
-        plc_list = storage.all(Place)
-        return jsonify([p.to_dict() for p in plc_list.values()])
-
-    # implicit else, return empty json.
-    plc_list = []
-    # also a proactive else for future if-conditions, as well
-
-    # If states list is not empty, results should include all Place objects
-    # for each State id listed
+        abort(400, 'Not a JSON')
+    st = bod_req.get('states')
 
     if st and len(st) > 0:
-        st_list = [storage.get("State", n) for n in st]
-        # json file of state names, including their ids
-
-        # iterate for every state in the states
-        for x in st_list:
-            # of every city in that state
-            for y in x.cities:
-                # for every place in that city
-                [plc_list.append(z) for z in y.places]
-                # add to the list of places and return as json,
-                # after all other if-blocks
-
+        cy_list = storage.all('City')
+        st_match_cy = set([city.id for city in cy_list.values()
+                            if city.state_id in st])
+    else:
+        st_match_cy = set()
+    
+    cy = bod_req.get('cy')
+    
     if cy and len(cy) > 0:
-        cy_list = [storage.get("City", n) for n in cy]
-        for x in cy_list:
-            [plc_list.append(y) for y in x.places if y not in plc_list]
-
-    if not plc_list:
-        plc_list = [n for n in storage.all(Place).values()]
-
-    # If amenities list is not empty, limit search results
-    # to only Place objects having all Amenity ids listed
-
-    if amn:
-        # create list of valid amenity instances
-        amn_list = [storage.get("Amenity", n) for n in amn]
-        count = 0
-        limit = len(plc_list)
-        HBNB_API_HOST = getenv('HBNB_API_HOST')
-        HBNB_API_PORT = getenv('HBNB_API_PORT')
-
-        port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
-        url = "http://0.0.0.0:{}/api/v1/places/".format(port)
-        while count < limit:
-            place = plc_list[count]
-            url = url + '{}/amenities'
-            req = url.format(place.id)
-            res = requests.get(req)
-            jlod = json.loads(res.text)
-            amen = [storage.get("Amenity", x['id']) for x in jlod]
-            for a in amn_list:
-                if a not in amen:
-                    plc_list.pop(count)
-                    count -= 1
-                    limit -= 1
-                    break
-            count += 1
-    # the proactive else
-    return jsonify([p.to_dict() for p in plc_list])
+        cy = set([
+            c_id for c_id in cy if storage.get('City', c_id)])
+        st_match_cy = st_match_cy.union(cy)
+    
+    amenities = bod_req.get('amenities')
+    
+    if len(st_match_cy) > 0:
+        plc_list = [p for p in plc_list if p.city_id in st_match_cy]
+        
+    elif amenities is None or len(amenities) == 0:
+        result = [place.to_json() for place in plc_list]
+        return jsonify(result)
+    
+    places_amenities = []
+    
+    if amenities and len(amenities) > 0:
+        amenities = set([
+            a_id for a_id in amenities if storage.get('Amenity', a_id)])
+        for p in plc_list:
+            p_amenities = None
+            if STORAGE_TYPE == 'db' and p.amenities:
+                p_amenities = [a.id for a in p.amenities]
+            elif len(p.amenities) > 0:
+                p_amenities = p.amenities
+            if p_amenities and all([a in p_amenities for a in amenities]):
+                places_amenities.append(p)
+    else:
+        places_amenities = plc_list
+    result = [place.to_json() for place in places_amenities]
+    return jsonify(result)
